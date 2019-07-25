5efb40b49087d5b46ba7b72ab793b2fbdcd31ee2
Radovan Bartanus <radovan.bartanus@flowup.cz>
2019-07-25 12:07:28 +0200

    asdasd

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello fren"))
+		w.Write([]byte("Heddllo fren"))
 		w.WriteHeader(http.StatusOK)
 	})
 	http.ListenAndServe(":80", nil)
